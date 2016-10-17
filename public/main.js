b60fb29c265719243a07349c1671eb8487a7505d
Meredith Nachman <mwnachman@gmail.com>
2016-10-17 12:45:01 -0700

    server is serving basic html file and paths are working

---
+++ public/main.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+	document.body.innerHTML += '<strong>Talk Javascript with Me</strong>'
+}
--- server/server.js
+++ server/server.js
@@ -1,33 +1,60 @@
 var http = require('http');
-var utils = require('utils');
+var fs = require('fs');
+var path = require('path');
+var utils = require('./utils');
 
 var port = 3000;
 
 
 // Angular has to run on lives-server, though, so not sure how to 
-// deal with this.
+// deal with this if I incorporate Angular.
 var ip = '127.0.0.1';
 
 
-// The function we pass to http.createServer will 
-//be used to handle all incoming requests.
-//
-// After creating the server, 
-//we will tell it to listen on the given port and IP. */
-//
-// We need to have a router to tell the server
-//what path to take                 
+var mimeLookup = {
+  '.js': 'application/javascript',
+  '.html': 'text/html'
+};
+
+
+// Instead have a router to tell the server
+// what path to take??                 
 
 
 
 var server = http.createServer( function(req, res) {
-  
-  // console.log('headers', req.headers);
-  // console.log('hello client');
-  res.write('writing');
-  res.end();
-
+  if (req.method === 'GET') {
+    var fileurl;
+    if (req.url === '/') {
+      fileurl = '/index.html';
+    } else {
+      fileurl = req.url;
+    }
+    var filepath = path.resolve('./public' + fileurl);
+    var fileExt = path.extname(filepath);
+    var mimeType = mimeLookup[fileExt];
+    if (!mimeType) {
+      utils.send404(res);
+      return;
+    }
+    fs.exists(filepath, function(exists) {
+      // console.log('filepath', filepath);
+      res.writeHead(200, {'Content-Type': mimeType});
+      fs.createReadStream(filepath).pipe(res);
+    });
+  } else {
+    utils.send404(res);
+  }
 });
 
 server.listen(port, ip);
 console.log('server listening');
+
+
+
+
+
+
+
+
+
